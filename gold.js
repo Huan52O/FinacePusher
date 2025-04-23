8284c409136f900fe9e372653ba002a22af4a485
Hello <clearhuan@qq.com>
2025-04-23 09:30:30 +0800

    fix: params

--- gold.js
+++ gold.js
@@ -1,39 +1,52 @@
-const axios = require('axios');
-const utils = require('./util');
+const axios = require("axios");
+const utils = require("./util");
 
-const { dateFormater } = utils
+const { dateFormater } = utils;
+
+const keyMap = {
+  f12: '代码',
+  f14: '名称',
+  f17: '今开',
+  f15: '最高',
+  f16: '最低',
+  f28: '昨结',
+  f124: '更新时间'
+}
 
 const getData = () => {
   const params = {
     np: 1,
     fltt: 1,
     invt: 2,
-    cb: `jQuery37105876305693407142_${new Date().getTime()}`,
-    fs: 'm:113+t:5',
-    fields: 'f12,f13,f14,f1,f2,f4,f3,f152,f17,f28,f15,f16,f5,f211,f212,f108',
-    fid: 'f3',
+    cb: `jQuery37107208853041718065_${new Date().getTime()}`,
+    fs: "m:118",
+    fields: "f12,f13,f14,f1,f2,f4,f3,f152,f17,f28,f15,f16,f124",
+    fid: "f3",
     pn: 1,
     pz: 5,
     po: 1,
-    ut: 'fa5fd1943c7b386f172d6893dbfba10b',
+    ut: "fa5fd1943c7b386f172d6893dbfba10b",
     dect: 1,
-    wbp2u: '|0|0|0|web',
-    _: `${new Date().getTime()}`
+    wbp2u: "|0|0|0|web",
+    _: `${new Date().getTime()}`,
   };
   return new Promise((resolve, reject) => {
-    axios.get('https://push2.eastmoney.com/api/qt/clist/get', { params })
-      .then(res => {
-        res.data ? resolve(res.data) : resolve([])
+    axios
+      .get("https://push2.eastmoney.com/api/qt/clist/get", { params })
+      .then((res) => {
+        res.data ? resolve(res.data) : resolve([]);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
-      })
-  })
+      });
+  });
 };
 
 const main = async () => {
   const res = await getData();
   console.log(res);
+  const data = JSON.parse(res.substring(res.indexOf("(") + 1, res.lastIndexOf(")")));
+  console.log(data);
 };
 
-main()
+main();
