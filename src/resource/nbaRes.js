e283d8670469a1428558e5e260ed8dfc7448bd00
GitHub Action <actions@github.com>
2025-07-17 02:26:56 +0000

    update nbaRes.js

--- src/resource/nbaRes.js
+++ src/resource/nbaRes.js
@@ -4,11 +4,88 @@ var nbaRes = [
     "value": 0,
     "children": [
       {
+        "name": "印第安纳步行者",
+        "value": 0,
+        "path": "EASTERN/印第安纳步行者",
+        "image": "https://ts4.cn.mm.bing.net/th?id=OSB.4rHQWRXbNIIpu3V_Ue0nYA--.png&pid=MSports&w=24&h=24&qlt=90&c=0&rs=1&dpr=2&p=1",
+        "rawName": "Indiana Pacers",
+        "record": "0-0",
+        "itemStyle": {
+          "color": "rgba(0, 123, 255, 0)"
+        }
+      },
+      {
+        "name": "芝加哥公牛",
+        "value": 0,
+        "path": "EASTERN/芝加哥公牛",
+        "image": "https://ts4.cn.mm.bing.net/th?id=OSB.6gQNbBlAX2iIA25R9YOI5Q--.png&pid=MSports&w=24&h=24&qlt=90&c=0&rs=1&dpr=2&p=1",
+        "rawName": "Chicago Bulls",
+        "record": "0-0",
+        "itemStyle": {
+          "color": "rgba(0, 123, 255, 0)"
+        }
+      },
+      {
+        "name": "克里夫兰骑士",
+        "value": 0,
+        "path": "EASTERN/克里夫兰骑士",
+        "image": "https://ts4.cn.mm.bing.net/th?id=OSB.eLd43AMqsoT_jZoPNCwtrA--.png&pid=MSports&w=24&h=24&qlt=90&c=0&rs=1&dpr=2&p=1",
+        "rawName": "Cleveland Cavaliers",
+        "record": "0-0",
+        "itemStyle": {
+          "color": "rgba(0, 123, 255, 0)"
+        }
+      },
+      {
+        "name": "底特律活塞",
+        "value": 0,
+        "path": "EASTERN/底特律活塞",
+        "image": "https://ts4.cn.mm.bing.net/th?id=OSB.WSnFrj2LiSMjYg7MWqhPSA--.png&pid=MSports&w=24&h=24&qlt=90&c=0&rs=1&dpr=2&p=1",
+        "rawName": "Detroit Pistons",
+        "record": "0-0",
+        "itemStyle": {
+          "color": "rgba(0, 123, 255, 0)"
+        }
+      },
+      {
         "name": "密尔沃基雄鹿",
         "value": 0,
         "path": "EASTERN/密尔沃基雄鹿",
         "image": "https://ts4.cn.mm.bing.net/th?id=OSB.6KDkdyns622Wb6nO1kY|xw--.png&pid=MSports&w=24&h=24&qlt=90&c=0&rs=1&dpr=2&p=1",
         "rawName": "Milwaukee Bucks",
+        "record": "0-0",
+        "itemStyle": {
+          "color": "rgba(0, 123, 255, 0)"
+        }
+      },
+      {
+        "name": "华盛顿奇才",
+        "value": 0,
+        "path": "EASTERN/华盛顿奇才",
+        "image": "https://ts4.cn.mm.bing.net/th?id=OSB.d2ytuL2SZ54vYoKTjFHOKA--.png&pid=MSports&w=24&h=24&qlt=90&c=0&rs=1&dpr=2&p=1",
+        "rawName": "Washington Wizards",
+        "record": "0-0",
+        "itemStyle": {
+          "color": "rgba(0, 123, 255, 0)"
+        }
+      },
+      {
+        "name": "夏洛特黄蜂",
+        "value": 0,
+        "path": "EASTERN/夏洛特黄蜂",
+        "image": "https://ts4.cn.mm.bing.net/th?id=OSB.kbcZsIrGf5NwpxjPj8RdZw--.png&pid=MSports&w=24&h=24&qlt=90&c=0&rs=1&dpr=2&p=1",
+        "rawName": "Charlotte Hornets",
+        "record": "0-0",
+        "itemStyle": {
+          "color": "rgba(0, 123, 255, 0)"
+        }
+      },
+      {
+        "name": "亚特兰大老鹰",
+        "value": 0,
+        "path": "EASTERN/亚特兰大老鹰",
+        "image": "https://ts4.cn.mm.bing.net/th?id=OSB.cbeNtSmc7sPvUruxPe9IYg--.png&pid=MSports&w=24&h=24&qlt=90&c=0&rs=1&dpr=2&p=1",
+        "rawName": "Atlanta Hawks",
         "record": "0-0",
         "itemStyle": {
           "color": "rgba(0, 123, 255, 0)"
@@ -26,11 +103,44 @@ var nbaRes = [
         }
       },
       {
+        "name": "奥兰多魔术",
+        "value": 0,
+        "path": "EASTERN/奥兰多魔术",
+        "image": "https://ts4.cn.mm.bing.net/th?id=OSB.gTSe9Y1Ev2gg4GW6L1ZhXw--.png&pid=MSports&w=24&h=24&qlt=90&c=0&rs=1&dpr=2&p=1",
+        "rawName": "Orlando Magic",
+        "record": "0-0",
+        "itemStyle": {
+          "color": "rgba(0, 123, 255, 0)"
+        }
+      },
+      {
+        "name": "波士顿凯尔特人",
+        "value": 0,
+        "path": "EASTERN/波士顿凯尔特人",
+        "image": "https://ts4.cn.mm.bing.net/th?id=OSB.6h6rJb1wEHcuYlz44vXS9A--.png&pid=MSports&w=24&h=24&qlt=90&c=0&rs=1&dpr=2&p=1",
+        "rawName": "Boston Celtics",
+        "record": "0-0",
+        "itemStyle": {
+          "color": "rgba(0, 123, 255, 0)"
+        }
+      },
+      {
         "name": "费城76人",
         "value": 0,
         "path": "EASTERN/费城76人",
         "image": "https://ts4.cn.mm.bing.net/th?id=OSB.51FAw5SArVWwiG1|YbF0vw--.png&pid=MSports&w=24&h=24&qlt=90&c=0&rs=1&dpr=2&p=1",
         "rawName": "Philadelphia 76ers",
+        "record": "0-0",
+        "itemStyle": {
+          "color": "rgba(0, 123, 255, 0)"
+        }
+      },
+      {
+        "name": "多伦多猛龙",
+        "value": 0,
+        "path": "EASTERN/多伦多猛龙",
+        "image": "https://ts4.cn.mm.bing.net/th?id=OSB.Nk4e86cNIHsVujzbbzRlyw--.png&pid=MSports&w=24&h=24&qlt=90&c=0&rs=1&dpr=2&p=1",
+        "rawName": "Toronto Raptors",
         "record": "0-0",
         "itemStyle": {
           "color": "rgba(0, 123, 255, 0)"
@@ -88,11 +198,88 @@ var nbaRes = [
         }
       },
       {
+        "name": "洛杉矶快船",
+        "value": 0,
+        "path": "WESTERN/洛杉矶快船",
+        "image": "https://ts4.cn.mm.bing.net/th?id=OSB.T8ZbatVydejV7KKuDuqYfA--.png&pid=MSports&w=24&h=24&qlt=90&c=0&rs=1&dpr=2&p=1",
+        "rawName": "LA Clippers",
+        "record": "0-0",
+        "itemStyle": {
+          "color": "rgba(255, 0, 0, 0)"
+        }
+      },
+      {
         "name": "金州勇士",
         "value": 0,
         "path": "WESTERN/金州勇士",
         "image": "https://ts4.cn.mm.bing.net/th?id=OSB.J5llJcE7Npist|UIsG357g--.png&pid=MSports&w=24&h=24&qlt=90&c=0&rs=1&dpr=2&p=1",
         "rawName": "Golden State Warriors",
+        "record": "0-0",
+        "itemStyle": {
+          "color": "rgba(255, 0, 0, 0)"
+        }
+      },
+      {
+        "name": "犹他爵士",
+        "value": 0,
+        "path": "WESTERN/犹他爵士",
+        "image": "https://ts4.cn.mm.bing.net/th?id=OSB.dSMH5xzYFS4xj0PTwtNGxA--.png&pid=MSports&w=24&h=24&qlt=90&c=0&rs=1&dpr=2&p=1",
+        "rawName": "Utah Jazz",
+        "record": "0-0",
+        "itemStyle": {
+          "color": "rgba(255, 0, 0, 0)"
+        }
+      },
+      {
+        "name": "丹佛掘金",
+        "value": 0,
+        "path": "WESTERN/丹佛掘金",
+        "image": "https://ts4.cn.mm.bing.net/th?id=OSB.SEVz|K9UGg4nU4jhJe|JBw--.png&pid=MSports&w=24&h=24&qlt=90&c=0&rs=1&dpr=2&p=1",
+        "rawName": "Denver Nuggets",
+        "record": "0-0",
+        "itemStyle": {
+          "color": "rgba(255, 0, 0, 0)"
+        }
+      },
+      {
+        "name": "波特兰开拓者",
+        "value": 0,
+        "path": "WESTERN/波特兰开拓者",
+        "image": "https://ts4.cn.mm.bing.net/th?id=OSB._W7Yiwvl2X_TOlvEuUcPVQ--.png&pid=MSports&w=24&h=24&qlt=90&c=0&rs=1&dpr=2&p=1",
+        "rawName": "Portland Trail Blazers",
+        "record": "0-0",
+        "itemStyle": {
+          "color": "rgba(255, 0, 0, 0)"
+        }
+      },
+      {
+        "name": "俄克拉荷马雷霆",
+        "value": 0,
+        "path": "WESTERN/俄克拉荷马雷霆",
+        "image": "https://ts4.cn.mm.bing.net/th?id=OSB.J_aoMicnftaHKDqphEjWsw--.png&pid=MSports&w=24&h=24&qlt=90&c=0&rs=1&dpr=2&p=1",
+        "rawName": "Oklahoma City Thunder",
+        "record": "0-0",
+        "itemStyle": {
+          "color": "rgba(255, 0, 0, 0)"
+        }
+      },
+      {
+        "name": "明尼苏达森林狼",
+        "value": 0,
+        "path": "WESTERN/明尼苏达森林狼",
+        "image": "https://ts4.cn.mm.bing.net/th?id=OSB.BTtXl7GY7fLpNxjtCp5zxQ--.png&pid=MSports&w=24&h=24&qlt=90&c=0&rs=1&dpr=2&p=1",
+        "rawName": "Minnesota Timberwolves",
+        "record": "0-0",
+        "itemStyle": {
+          "color": "rgba(255, 0, 0, 0)"
+        }
+      },
+      {
+        "name": "休斯顿火箭",
+        "value": 0,
+        "path": "WESTERN/休斯顿火箭",
+        "image": "https://ts4.cn.mm.bing.net/th?id=OSB.TvWeZIilKkK8hws2WrSFSA--.png&pid=MSports&w=24&h=24&qlt=90&c=0&rs=1&dpr=2&p=1",
+        "rawName": "Houston Rockets",
         "record": "0-0",
         "itemStyle": {
           "color": "rgba(255, 0, 0, 0)"
